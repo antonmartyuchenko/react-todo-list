f29bf88a41dfb1abedd36f5bfc64db49afbf8a30
Anton Martyuchenko <antonmartyuchenko@gmail.com>
2020-07-07 10:06:17 +0300

    cross-origin error

--- src/components/ToDoList/TasksTable.jsx
+++ src/components/ToDoList/TasksTable.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Table } from 'react-bootstrap';
 
-const TasksTable = ({tasks}) => {
-
+const TasksTable = ({ deleteMessage, tasks }) => {
     return (
         <div>
-            <table id="tasksTable">
-                {tasks.map(task => <tr><td className="td">{task}</td></tr>)}
-            </table>
+            <Table id="tasksTable" variant="dark">
+                <tr><th>Message</th></tr>
+                {tasks.map((task, index) => {
+                    //console.log(task);
+                    const { message } = JSON.parse(task);
+
+                    return <tr>
+                        <td className="td">{message}</td>
+                        <span class="glyphicon glyphicon-remove" onClick={deleteMessage}><span class="hidden">{index}</span></span>
+                    </tr>
+                })}
+            </Table>
         </div>
     );
-
 }
+
 
 TasksTable.propTypes = {
     tasks: PropTypes.array.isRequired
--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,39 +1,59 @@
 import React from 'react';
+import axios from 'axios';
 import TasksTable from './TasksTable';
+import { Button, InputGroup, FormControl } from 'react-bootstrap';
 
-class ToDoList extends React.PureComponent  {
+class ToDoList extends React.PureComponent {
 
   state = {
     inputTaskValue: '',
     tasks: []
   }
 
-  setTaskValue = e => { 
-    this.setState({inputTaskValue: e.target.value});
+  componentDidMount() {
+    this.setState({ tasks: [{id: 1, message: 'test'}] }); 
+    /*axios.get(`http://127.0.0.1:3001/api/tasks`).then(res => {
+    console.log(res.data);  
+    this.setState({ tasks: res.data });
+    })*/
+  }
+
+  setTaskValue = e => {
+    this.setState({ inputTaskValue: e.target.value });
   }
 
   addTask = () => {
-
-    const {inputTaskValue} = this.state;
+    const { inputTaskValue } = this.state;
 
     if (inputTaskValue !== '') {
-      this.setState({tasks: [...this.state.tasks, inputTaskValue]});
-    } 
-  
-    this.state.inputTaskValue = '';
+      
 
+      axios.post(`http://127.0.0.1:3001/api/tasks`, { message: inputTaskValue }).then(res => {
+        this.setState({ tasks: [...this.state.tasks, res.data] });
+      })
+
+      this.setState({ inputTaskValue: '' });
+    }
+  }
+
+  deleteMessage = e => {
+    console.log(e);
   }
 
   render() {
-   
     return (
-      <>
-        <input className="inputTask" onChange={this.setTaskValue} value={this.state.inputTaskValue}></input>
-        <button className="addButton" onClick={this.addTask}>Add task</button>
-        <TasksTable tasks={this.state.tasks}/>
-      </>
+      <div id="div">
+        <InputGroup onChange={this.setTaskValue} value={this.state.inputTaskValue}>
+          <FormControl
+            placeholder="Message"
+          />
+          <InputGroup.Append>
+            <Button variant="primary" onClick={this.addTask}>Add task</Button>
+          </InputGroup.Append>
+        </InputGroup>
+        <TasksTable deleteMessage={this.deleteMessage} tasks={this.state.tasks} />
+      </div>
     );
-
   }
 }
 
